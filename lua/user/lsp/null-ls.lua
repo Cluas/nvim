0c3eb0fda6dfa653e19e431943785401c6b61932
Cluas <Cluas@live.cn>
2022-03-26 01:18:24 +0800

    feat: add buf lint format

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -15,7 +15,8 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.goimports,
-		diagnostics.protoc_gen_lint,
+		formatting.buf,
 		diagnostics.golangci_lint,
+		diagnostics.buf,
 	},
 })
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -87,7 +87,7 @@ return packer.startup(function(use)
 	use("neovim/nvim-lspconfig") -- enable LSP
 	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
 	use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
-	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+	use("Cluas/null-ls.nvim") -- for formatters and linters
 	use("ray-x/lsp_signature.nvim")
 	use("github/copilot.vim") -- ai completions
 	use({

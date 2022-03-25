f751ad80f909539437a11d1e1f5380cb0d50dea0
Cluas <Cluas@live.cn>
2022-03-26 03:43:36 +0800

    fix: tab error

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -77,11 +77,8 @@ cmp.setup({
 		-- Set `select` to `false` to only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping(function(fallback)
-			local copilot_keys = vim.fn["copilot#Accept"]()
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif copilot_keys ~= "" then
-				vim.api.nvim_feedkeys(copilot_keys, "i", true)
 			elseif luasnip.expandable() then
 				luasnip.expand()
 			elseif luasnip.expand_or_jumpable() then

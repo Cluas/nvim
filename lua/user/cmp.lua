97b0d3f5d4f35ccd1bc637b644836446fdb91955
cluas <cluas@live.cn>
2022-04-17 16:05:55 +0800

    feat: update lsp

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,5 +1,5 @@
-local ok, cmp = pcall(require, "cmp")
-if not ok then
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
 	return
 end
 
@@ -8,55 +8,18 @@ if not snip_status_ok then
 	return
 end
 
--- load vscode snippets
 require("luasnip/loaders/from_vscode").lazy_load()
-
--- load custom snippets if they exist
-if vim.fn.empty(vim.fn.glob("~/.config/snippets")) == 0 then
-	require("luasnip/loaders/from_vscode").lazy_load({ paths = { "~/.config/snippets" } })
-end
 
 local check_backspace = function()
 	local col = vim.fn.col(".") - 1
 	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
---   פּ ﯟ   some other good icons
-local kind_icons = {
-	Text = "",
-	Method = "m",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
-}
--- find more here: https://www.nerdfonts.com/cheat-sheet
+local icons = require("user.icons")
+
+local kind_icons = icons.kind
 
 cmp.setup({
-	appearance = {
-		menu = {
-			direction = "above", -- auto or above or below
-		},
-	},
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body) -- For `luasnip` users.
@@ -68,7 +31,7 @@ cmp.setup({
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+		-- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mappng.
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
@@ -110,28 +73,47 @@ cmp.setup({
 		format = function(entry, vim_item)
 			-- Kind icons
 			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+
+			if entry.source.name == "cmp_tabnine" then
+				-- if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+				-- menu = entry.completion_item.data.detail .. " " .. menu
+				-- end
+				vim_item.kind = icons.misc.Robot
+			end
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			-- NOTE: order matters
 			vim_item.menu = ({
-				copilot = "[Copilot]",
-				nvim_lsp = "[LSP]",
-				luasnip = "[Snippet]",
-				buffer = "[Buffer]",
-				path = "[Path]",
+				-- nvim_lsp = "[LSP]",
+				-- nvim_lua = "[Nvim]",
+				-- luasnip = "[Snippet]",
+				-- buffer = "[Buffer]",
+				-- path = "[Path]",
+				-- emoji = "[Emoji]",
+
+				nvim_lsp = "",
+				nvim_lua = "",
+				luasnip = "",
+				buffer = "",
+				path = "",
+				emoji = "",
 			})[entry.source.name]
 			return vim_item
 		end,
 	},
 	sources = {
-		{ name = "copilot" },
 		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
+		{ name = "cmp_tabnine" },
 		{ name = "path" },
+		{ name = "emoji" },
 	},
 	confirm_opts = {
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
+	-- documentation = true,
 	window = {
 		-- documentation = "native",
 		documentation = {
@@ -139,13 +121,7 @@ cmp.setup({
 		},
 	},
 	experimental = {
-		ghost_text = false,
-		native_menu = false,
-	},
-})
-
-cmp.setup.filetype("release-note", {
-	sources = {
-		{ name = "luasnip" },
+		ghost_text = true,
+		-- native_menu = false,
 	},
 })

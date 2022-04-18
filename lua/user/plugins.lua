e4ab3d072ba259e97c4010a7e02eb88464884de3
Cluas <Cluas@live.cn>
2022-04-19 02:31:17 +0800

    feat: add lua doc

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -121,6 +121,10 @@ return packer.startup(function(use)
 	use("Pocco81/DAPInstall.nvim")
 	use("leoluz/nvim-dap-go")
 
+  -- Lua
+  use("nanotee/luv-vimdocs")
+  use("milisims/nvim-luaref")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then

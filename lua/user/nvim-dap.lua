1dc24e71088d9ab8b02b558234e8710d91c8510b
pseudoyu <pseudoyu@connect.hku.hk>
2022-06-18 21:59:07 +0800

    fix: 'sidebar' and 'tray' deprecated issue

--- lua/user/nvim-dap.lua
+++ lua/user/nvim-dap.lua
@@ -99,25 +99,27 @@ dapui.setup({
 		edit = "e",
 		repl = "r",
 	},
-	sidebar = {
-		-- You can change the order of elements in the sidebar
-		elements = {
-			-- Provide as ID strings or tables with "id" and "size" keys
-			{
-				id = "scopes",
-				size = 0.25, -- Can be float or integer > 1
+	layouts = {
+		{
+			-- You can change the order of elements in the layout
+			elements = {
+				-- Provide as ID strings or tables with "id" and "size" keys
+				{
+					id = "scopes",
+					size = 0.25, -- Can be float or integer > 1
+				},
+				{ id = "breakpoints", size = 0.25 },
+				{ id = "stacks", size = 0.25 },
+				{ id = "watches", size = 0.25 },
 			},
-			{ id = "breakpoints", size = 0.25 },
-			{ id = "stacks", size = 0.25 },
-			{ id = "watches", size = 0.25 },
+			size = 40,
+			position = "left", -- Can be "left", "right", "top", "bottom"
 		},
-		size = 40,
-		position = "left", -- Can be "left", "right", "top", "bottom"
-	},
-	tray = {
-		elements = { "repl" },
-		size = 10,
-		position = "bottom", -- Can be "left", "right", "top", "bottom"
+		{
+			elements = { "repl" },
+			size = 10,
+			position = "bottom", -- Can be "left", "right", "top", "bottom"
+		},
 	},
 	floating = {
 		max_height = nil, -- These can be integers or a float between 0 and 1.

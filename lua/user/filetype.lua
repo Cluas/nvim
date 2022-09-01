a85d953f560d50f92c999f304f58ebcfc93f682f
Cluas <Cluas@live.cn>
2022-09-01 14:16:14 +0800

    feat: add Dockerfile filetype detect

--- lua/user/filetype.lua
+++ lua/user/filetype.lua
@@ -1,12 +1,26 @@
-vim.g.do_filetype_lua=1
+vim.g.do_filetype_lua = 1
 vim.filetype.add({
-  extension = {
-    txt = function(_, _)
-      local filename =  vim.fn.expand('%:t:r')
-      if filename:match('%d') then
-        return "release-note"
-      end
-      return "txt"
-    end,
-  }
+	extension = {
+		txt = function(_, _)
+			local filename = vim.fn.expand("%:t:r")
+			if filename:match("%d") then
+				return "release-note"
+			end
+			return "txt"
+		end,
+		builder = function(_, _)
+			local filename = vim.fn.expand("%:t:r")
+			if filename == "Dockerfile" then
+				return "dockerfile"
+			end
+			return "builder"
+		end,
+		lb = function(_, _)
+			local filename = vim.fn.expand("%:t:r")
+			if filename == "Dockerfile" then
+				return "dockerfile"
+			end
+			return "lb"
+		end,
+	},
 })

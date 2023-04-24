72b17cee9678d3806b6543bdbcda6e900e8428bb
Cluas <Cluas@live.cn>
2023-04-24 14:58:36 +0800

    feat: upgrade nvim-tree.lua

--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -14,7 +14,25 @@ local icons = require("user.icons")
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- Mappings migrated from view.mappings.list
+	--
+	-- You will need to insert "your code goes here" for any mappings with a custom action_cb
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "h", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+end
+
 nvim_tree.setup({
+	on_attach = on_attach,
 	hijack_directories = {
 		enable = false,
 	},
@@ -93,14 +111,6 @@ nvim_tree.setup({
 		width = 30,
 		hide_root_folder = false,
 		side = "left",
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-				{ key = "h", action = "edit" },
-				{ key = "v", cb = tree_cb("vsplit") },
-			},
-		},
 		number = false,
 		relativenumber = false,
 	},
